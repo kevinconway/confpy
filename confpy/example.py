969f9d8d9e09e90530c9dcd16772746405338760
Kevin Conway <kevinjacobconway@gmail.com>
2015-01-05 20:52:54 -0600

    Add example configuration file generator
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

---
+++ confpy/example.py
@@ -0,0 +1,63 @@
+"""Configuration file example generator."""
+
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+
+if sys.version_info[0] == 3 and sys.version_info[1] == 2:
+
+    raise ImportError(
+        "Example generator cannot be imported in Python 3.2.X."
+    )
+
+import jinja2
+
+
+ENV = jinja2.Environment(
+    loader=jinja2.PackageLoader('confpy', 'templates'),
+)
+
+
+def generate_example_ini(config):
+    """Generate an INI file based on the given Configuration object.
+
+    Args:
+        config (confpy.core.configuration.Configuration): The configuration
+            object on which to base the example.
+
+    Returns:
+        str: The text of the example INI file.
+    """
+    return generate_example(config, ext='INI')
+
+
+def generate_example_json(config):
+    """Generate an JSON file based on the given Configuration object.
+
+    Args:
+        config (confpy.core.configuration.Configuration): The configuration
+            object on which to base the example.
+
+    Returns:
+        str: The text of the example JSON file.
+    """
+    return generate_example(config, ext='JSON')
+
+
+def generate_example(config, ext='json'):
+    """Generate an example file based on the given Configuration object.
+
+    Args:
+        config (confpy.core.configuration.Configuration): The configuration
+            object on which to base the example.
+        ext (str): The file extension to render. Choices: JSON and INI.
+
+    Returns:
+        str: The text of the example file.
+    """
+    template_name = 'example.{0}'.format(ext.lower())
+    template = ENV.get_template(template_name)
+    return template.render(config=config)
---
+++ tests/test_example.py
@@ -0,0 +1,66 @@
+"""Test suite for generating example config files."""
+
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import pytest
+
+example = pytest.importorskip("confpy.example")
+from confpy.core import config
+from confpy.core import namespace
+from confpy.options import boolopt
+from confpy.options import numopt
+from confpy.options import stropt
+
+
+@pytest.fixture(scope='module')
+def cfg():
+    """Get a configuration object with options set."""
+    return config.Configuration(
+        example_test_section=namespace.Namespace(
+            description="section",
+            example_empty=boolopt.BoolOption(),
+            example_bool=boolopt.BoolOption(default=True, description='bool'),
+            example_number=numopt.IntegerOption(default=10, description='int'),
+            example_string=stropt.StringOption(default='a', description='str'),
+        ),
+        example_test_section_two=namespace.Namespace(
+            description="section2",
+            example_empty_two=boolopt.BoolOption(),
+        ),
+    )
+
+
+def test_example_generator_ini(cfg):
+    """Check if INI files are rendered appropriately."""
+    text = example.generate_example_ini(cfg)
+    assert text == """# section
+[example_test_section]
+example_bool = True # bool
+example_empty =
+example_number = 10 # int
+example_string = a # str
+
+# section2
+[example_test_section_two]
+example_empty_two =
+
+"""
+
+
+def test_example_generator_json(cfg):
+    """Check if JSON files are rendered appropriately."""
+    text = example.generate_example_json(cfg)
+    assert text == """{
+   "example_test_section": {
+        "example_bool": "True",
+        "example_empty": null,
+        "example_number": "10",
+        "example_string": "a"
+    },
+   "example_test_section_two": {
+        "example_empty_two": null
+    }
+}"""

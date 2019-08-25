67e65049b021e054d71fe6bee050a0ebb051970a
Kevin Conway <kevinjacobconway@gmail.com>
2019-08-24 19:35:44 -0500

    Update test automation scripts

--- setup.py
+++ setup.py
@@ -4,25 +4,24 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-with open('README.rst', 'r') as readmefile:
+with open("README.rst", "r") as readmefile:
 
     README = readmefile.read()
 
 setup(
-    name='confpy',
-    version='0.10.0',
-    url='https://github.com/kevinconway/confpy',
-    description='Config file parsing and option management.',
+    name="confpy",
+    version="0.10.0",
+    url="https://github.com/kevinconway/confpy",
+    description="Config file parsing and option management.",
     author="Kevin Conway",
     author_email="kevinjacobconway@gmail.com",
     long_description=README,
-    license='MIT',
-    packages=find_packages(exclude=['tests', 'build', 'dist', 'docs']),
+    license="MIT",
+    packages=find_packages(exclude=["tests", "build", "dist", "docs"]),
     install_requires=[],
+    extras_require={"generator": ["Jinja2"]},
     entry_points={
-        'console_scripts': [
-            'confpy-generate = confpy.cmd:generate_example',
-        ],
+        "console_scripts": ["confpy-generate = confpy.cmd:generate_example"]
     },
     include_package_data=True,
 )
